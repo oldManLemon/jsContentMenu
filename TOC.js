ea847634aa01b29a9949fc9a23f80214b304ab88
Andrew Hase <oldm8reggie@gmail.com>
2018-04-26 11:18:51 +1000

    Working for Strange Wordpress design, need to change anchor point links to <ap> tags so we can gather data correctly

--- TOC.js
+++ TOC.js
@@ -1,8 +1,10 @@
 var h1Counter = document.getElementsByTagName("h1"); //Gather all elements with the tag
+var anchorPointCounter = document.getElementsByTagName("ap");
+console.log(anchorPointCounter);
 var nav = document.getElementById('toc');
-var navbarWrite = nav.innerHTML;
-for (i = 0; i <= h1Counter.length; i++) {
-    var titles = h1Counter[i].innerHTML;
-    var links = "#" + h1Counter[i].attributes.id.value;
-    navbarWrite += nav.innerHTML += '<li class="nav-item"><a class="nav-link" href="' + links + '">' + titles + '</a></li>';
-    }
+var navbarWrite = nav.innerHTML = '<h2><a href="http://bwwiki:49494/handbooks/revitHandbook.html">Main Contents</a></h2>';
+for (i = 1; i <= h1Counter.length; i++) {
+  var titles = h1Counter[i].innerText;
+  var links = '#' + anchorPointCounter[i - 1].id;
+  navbarWrite += nav.innerHTML += '<li class="nav-item"><a class="nav-link" href="' + links + '">' + titles + '</a></li>';
+}
